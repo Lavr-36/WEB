21f6c4a78664927b1ef11559e6670f9bc31c8aba
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-07-22 17:54:46 +0300

    TODO: place Factoeial result

---
+++ JavaScript/forms_and_events.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function Factorial()
+{
+	let numberElement = document.getElementById("number");
+	let number = numberElement.value;
+	let resultElement = document.getElementById("factorial-result");
+	let f = BigInt(1);
+	for (let i = 1n; i <= number; i++)
+	{
+		f *= i;
+	}
+	//resultElement.value = `${number}! = ${f}`;
+	resultElement.innerHTML = `${number}! = ${f}`;
+}
